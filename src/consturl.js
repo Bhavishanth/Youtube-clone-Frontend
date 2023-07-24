6542bd41c41a34e9dbfa6f078afdc0d21f09b398
bhavishanth <bhavishanth@gmail.com>
2023-07-24 13:20:42 +0530

    first commit

---
+++ src/consturl.js
@@ -0,0 +1,3 @@
+export const be_url = "https://yt-be-server.onrender.com/api";
+
+// "proxy": "http://localhost:8800/api/"
---
+++ src/pages/Home.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import Card from "../components/Card";
+import axios from "axios";
+import { be_url } from "../consturl";
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+`;
+
+const Home = ({type}) => {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const fetchVideos = async () => {
+      const res = await axios.get(`${be_url}/videos/${type}`);
+      setVideos(res.data);
+    };
+    fetchVideos();
+  }, [type]);
+
+  return (
+    <Container>
+      {videos.map((video) => (
+        <Card key={video._id} video={video}/>
+      ))}
+    </Container>
+  );
+};
+
+export default Home;
